2087be9b2be98d9b4183cd169a3eca52180fdb8c
dykim <drdragon777@whatap.io>
2024-08-14 00:59:05 +0900

    chore: window.open 대신 navigate로 링크 이동 수정

--- apps/web/src/libs/app-download-click.ts
+++ apps/web/src/libs/app-download-click.ts
@@ -6,7 +6,7 @@ import { detectDevice } from "./detect-device";
 export const onAppDownloadClick = (navigate: (route: string) => void) => {
   // TODO: 앱이 있으면 앱 열기, 없으면 playstore로 이동
   const device = detectDevice();
-  if (device === "android") window.open(AOS_APP_LINK);
+  if (device === "android") navigate(AOS_APP_LINK);
   else if (device !== null) navigate("/ios-not-supported");
   if (device !== null) window.clarity?.("event", `install-modal-${device}`);
 };
